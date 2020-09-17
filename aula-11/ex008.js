ae376ee00e7bd96b88233106c5ba951b65559f92
thiagonmiziara <thiagonmiziara@gmail.com>
2020-09-17 18:43:01 -0300

    aprendendo condicionais if e else

---
+++ aula-11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 50.5;
+console.log(`A velociada do seu carro é ${vel}Km/h`);
+if (vel > 60) { // condição simples!
+    console.log('Você ultrapassou a velocidade permitida. Multado!  ');
+}
+console.log('Dirija com cuidado!');
