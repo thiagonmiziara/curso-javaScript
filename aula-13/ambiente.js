9c5f1ebf8d188c7cba2e86a419cca391b122311a
thiagonmiziara <thiagonmiziara@gmail.com>
2020-09-18 21:55:29 -0300

    estrutura de repetição while e do while

---
+++ aula-13/ambiente.js
@@ -0,0 +1,16 @@
+let contador = 1
+do {
+    console.log(`Passo ${contador}`);
+    contador++;
+} while (contador <= 6);
+
+
+
+
+/*
+let contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`);
+    contador++;
+};
+*/
